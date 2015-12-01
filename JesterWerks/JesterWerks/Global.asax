407e0ad1652a43d024b5180c49d3e663aecc240b
JesterWerks <jesterwerks@gmail.com>
2015-12-01 16:02:14 -0600

    Initial site load

---
+++ JesterWerks/JesterWerks/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JesterWerks.MvcApplication" Language="C#" %>
---
+++ JesterWerks/JesterWerks/Scripts/VM/HomeIndexVM.js
@@ -0,0 +1,24 @@
+﻿//@@ sourceURL=VM/HomeIndexVM.js
+
+jQuery(document).ready(function () {
+    BindViewModel();
+
+});
+
+var Home = function () {
+    var self = this;
+
+};
+
+function BindViewModel() {
+
+    // create instance of model
+    var vm = new Home();
+
+    // setup validation for instance
+    //vm.errors = ko.validation.group(vm, { deep: true });
+
+    // apply bindings
+    ko.applyBindings(vm);
+
+};
